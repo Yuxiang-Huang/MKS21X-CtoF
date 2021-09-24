fd617fdcb89d49346cac1467b0e84d2de1908dd7
Yuxiang Huang <yhuang30@stuy.edu>
2021-09-23 21:18:18 -0400

    Wrote second function

--- CtoFTester.java
+++ CtoFTester.java
@@ -5,4 +5,7 @@ public class CtoFTester{
 	public static double celsiusToFahrenheit(double c){
 		return c * 9/5 + 32;
 	}
+	public static double fahrenheitToCelsius(double f){
+                return (f - 32) * 5/9;
+        }
 }
