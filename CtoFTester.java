422f07552a68d80b6e645e84cb8e8e7af1c18aa4
Yuxiang Huang <yhuang30@stuy.edu>
2021-09-23 21:12:32 -0400

    Wrote first function

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,8 @@
-
+public class CtoFTester{
+	/* The functions should have doubles that represent the degrees in 
+	Celsius/Fahrenheit as parameters. The functions should return doubles
+	*/
+	public static double celsiusToFahrenheit(double c){
+		return c * 9/5 + 32;
+	}
+}
